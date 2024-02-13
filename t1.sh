304beb3436308f5e39cf888a49210121d97f9d24
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2024-02-13 01:28:24 -0800

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -1,7 +1,20 @@
 ADDUSER=''
 
+function add() {
+        echo -e "     ${CYAN}Add a new user?"
+        echo "       1) Yes"
+        echo -e "       2) No${CN}"
+        read -p "     " n
+        case $n in
+        1) ADD=1;;
+        2) ADD=2;;
+        *) echo -e "     ${RED}invalid option selected.. :( try again${CN}"  ;add;
+        esac
+        echo " "
+}
+
 function adduser() {
-    if [ $PACK == 1 ]
+    if [ $ADD == 1 ]
         then
     echo -e "${YELLOW}Add a new user{NC}" && sleep 1
     read -p "     New User Name: " ADDUSER
@@ -10,4 +23,5 @@ function adduser() {
 
 #MAIN
 
+add
 adduser
