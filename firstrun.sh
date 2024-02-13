db598d3823435fb67c3d83a409bff8a6dca40407
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2024-02-13 01:18:42 -0800

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -12,6 +12,8 @@ STOP='\e[0m'
 ADDUSER=''
 
 function adduser() {
+    if [ $PACK == 1 ]
+        then
     echo -e "${YELLOW}Add a new user{NC}" && sleep 1
     read -p "     New User Name: " ADDUSER
     adduser $ADDUSER
