b2bf268e7c8acc596da004f78c4e76655bc9c8d5
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2024-02-15 13:37:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -162,21 +162,21 @@ function download_node() {
                 then
                 mkdir temp
                 curl -L $WALLET_TAR_U_22 | tar xz -C ./temp;
-                mv temp/* ~/
+                mv -f temp/* ~/
                 rm -r temp
 
         elif [ $osType == "x86_64" ] && [ $VERSION_ID == "20.04" ]
                 then
                 mkdir temp
                 curl -L $WALLET_TAR_U_20 | tar xz -C ./temp;
-                mv temp/* ~/
+                mv -f temp/* ~/
                 rm -r temp
 
         elif [ $osType == "aarch64" ]
                 then
                 mkdir temp
                 curl -L $WALLET_TAR_ARM_64 | tar xz -C ./temp;
-                mv temp/* ~/
+                mv -f temp/* ~/
                 rm -r temp
 
         fi
